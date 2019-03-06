e14732a8f3a17b18d601fb9361f37dccd713d3da
Satya <satyanarayan.chakiri@mavs.uta.edu>
2019-03-06 15:15:24 -0600

    r1

--- main.py
+++ main.py
@@ -6,14 +6,5 @@ app = Flask(__name__)
 def my_form():
     return render_template('my-form.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return ( 'Welcome, 😊 ' + processed_text + ' 😊 to our Cloud Computing Summar 2018 class')
-
-def hello_world():
-  return 'Hello, World!\n This looks just amazing within 5 minutes'
-
 if __name__ == '__main__':
   app.run()
